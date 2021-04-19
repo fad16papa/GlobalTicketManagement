e1eabfbe4b6de9f2264290be2ca35cc0031b63a0
Francis Decena <fad16papa@gmail.com>
2021-04-19 17:46:11 +0800

    Commit adding xUnitTest

---
+++ GlobalTicket.TicketManagement.Application.UnitTests/Categories/Commands/CreateCategoryTests.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using GlobalTicket.TicketManagement.Application.Contracts.Persistence;
+using GlobalTicket.TicketManagement.Application.Features.Categories.Commands.CreateCategory;
+using GlobalTicket.TicketManagement.Application.Profiles;
+using GlobalTicket.TicketManagement.Application.UnitTests.Mocks;
+using GlobalTicket.TicketManagement.Domain.Entities;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GlobalTicket.TicketManagement.Application.UnitTests.Categories.Commands
+{
+    public class CreateCategoryTests
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<IAsyncRepository<Category>> _mockCategoryRepository;
+
+        public CreateCategoryTests()
+        {
+            _mockCategoryRepository = RepositoryMocks.GetCategoryRepository();
+            var configurationProvider = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+
+            _mapper = configurationProvider.CreateMapper();
+        }
+
+        [Fact]
+        public async Task Handle_ValidCategory_AddedCategoriesRepo()
+        {
+            var handler = new CreateCategoryCommandHandler(_mapper, _mockCategoryRepository.Object);
+
+            await handler.Handle(new CreateCategoryCommand()
+            {
+                Name = "Test"
+            }, CancellationToken.None);
+
+            var allCategories = await _mockCategoryRepository.Object.ListAllAsync();
+            allCategories.Count.ShouldBe(5);
+        }
+    }
+}
